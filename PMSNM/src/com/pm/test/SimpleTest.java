91b2c1a84c700d44c237cf1c9c054cc0614e2de1
mallikar <mallikar@IN-7SMH5H2.mshome.net>
2019-05-17 18:57:05 +0530

    Commit

--- PMSNM/src/com/pm/test/SimpleTest.java
+++ PMSNM/src/com/pm/test/SimpleTest.java
@@ -1,8 +1,11 @@
 package com.pm.test;
 
+import org.testng.annotations.Test;
+
 public class SimpleTest 
 {
-	public static void main(String[] args) 
+	@Test
+	public void main() 
 	{
 		System.out.println("Hello");
 	}
